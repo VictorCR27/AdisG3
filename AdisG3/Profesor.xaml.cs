d3af4022ef8ad20c611ff57f827e0a15727a39ed
Victor Segura <danielsegurajaime@gmail.com>
2023-07-14 19:53:24 -0600

    Cambios en semanas

--- AdisG3/Profesor.xaml.cs
+++ AdisG3/Profesor.xaml.cs
@@ -1,115 +1,139 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace AdisG3
 {
-    /// <summary>
-    /// Interaction logic for Profesor.xaml
-    /// </summary>
     public partial class Profesor : Window
     {
         public int id_profesor { get; set; }
         public int id_cursoSeleccionado { get; set; }
         public string nombreCursoSeleccionado { get; set; }
+
+
         public Profesor(int id_profesor = 0, int id_cursoSeleccionado = 0, string nombreCursoSeleccionado = "")
         {
             InitializeComponent();
 
             this.id_profesor = id_profesor;
             this.id_cursoSeleccionado = id_cursoSeleccionado;
-            //MessageBox.Show(id_cursoSeleccionado.ToString());
+            this.nombreCursoSeleccionado = nombreCursoSeleccionado;
+
             curso.Content = nombreCursoSeleccionado;
+
+            cbox_semana.SelectionChanged += ComboBox_SelectionChanged_1;
+
+            CargarCursosPublicados();
+            CargarSemanas();
         }
 
-        private void Button_sem1(object sender, RoutedEventArgs e)
+        private void CargarCursosPublicados()
         {
-            Semanas_Frame1.NavigationService.Navigate(new Semanas.Pag_sem1());
+            string connString = conn_db.GetConnectionString(); // Reemplaza con tu cadena de conexión
+
+            using (MySqlConnection connection = new MySqlConnection(connString))
+            {
+                connection.Open();
+
+                // Crea la consulta SQL para obtener los cursos publicados por el profesor
+                string query = "SELECT DISTINCT c.nombre_curso FROM cursos AS c " +
+                               "INNER JOIN asignacionesSemanas AS a ON c.id_curso = a.id_curso " +
+                               "WHERE a.id_profesor = @id_profesor";
+
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    // Asigna el valor al parámetro de la consulta
+                    command.Parameters.AddWithValue("@id_profesor", id_profesor);
+
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        List<string> cursos = new List<string>();
+
+                        while (reader.Read())
+                        {
+                            string curso = reader.GetString("nombre_curso");
+                            cursos.Add(curso);
+                        }
+
+                        lvAsignacionesSemana.ItemsSource = cursos;
+                    }
+                }
+            }
         }
 
-        private void Button_sem2(object sender, RoutedEventArgs e)
+        private void CargarSemanas()
         {
-            Semanas_Frame1.NavigationService.Navigate(new Semanas.Pag_sem2());
+
+            string connString = conn_db.GetConnectionString(); // Reemplaza con tu cadena de conexión
+
+            using (MySqlConnection connection = new MySqlConnection(connString))
+            {
+                connection.Open();
+
+                // Crea la consulta SQL para obtener las semanas
+                string query = "SELECT DISTINCT semana FROM asignacionesSemanas WHERE id_profesor = @id_profesor AND id_curso = @id_curso";
+
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    // Asigna los valores a los parámetros de la consulta
+                    command.Parameters.AddWithValue("@id_profesor", id_profesor);
+                    command.Parameters.AddWithValue("@id_curso", id_cursoSeleccionado);
+
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+
+                        for (int semana = 1; semana <= 15; semana++)
+                        {
+                            cbox_semana.Items.Add(semana);
+                        }
+                    }
+                }
+            }
         }
 
-        private void Button_sem3(object sender, RoutedEventArgs e)
+        private void CargarAsignacionesSemana(int semana)
         {
-            Semanas_Frame1.NavigationService.Navigate(new Semanas.Pag_sem3());
+            string connString = conn_db.GetConnectionString(); // Reemplaza con tu cadena de conexión
+
+            using (MySqlConnection connection = new MySqlConnection(connString))
+            {
+                connection.Open();
+
+                // Crea la consulta SQL para obtener las asignaciones de la semana seleccionada
+                string query = "SELECT titulo, tipo, descripcion, FechaEntrega, valor FROM asignacionesSemanas " +
+                               "WHERE id_profesor = @id_profesor AND id_curso = @id_curso AND semana = @semana";
+
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    // Asigna los valores a los parámetros de la consulta
+                    command.Parameters.AddWithValue("@id_profesor", id_profesor);
+                    command.Parameters.AddWithValue("@id_curso", id_cursoSeleccionado);
+                    command.Parameters.AddWithValue("@semana", semana);
+
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        DataTable dataTable = new DataTable();
+                        dataTable.Load(reader);
+
+                        lvAsignacionesSemana.ItemsSource = dataTable.DefaultView;
+                    }
+                }
+            }
         }
 
-        private void Button_sem4(object sender, RoutedEventArgs e)
+        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
         {
-            Semanas_Frame1.NavigationService.Navigate(new Semanas.Pag_sem4());
-        }
-
-        private void Button_sem5(object sender, RoutedEventArgs e)
-        {
-            Semanas_Frame1.NavigationService.Navigate(new Semanas.Pag_sem5());
-        }
-
-        private void Button_sem6(object sender, RoutedEventArgs e)
-        {
-            Semanas_Frame1.NavigationService.Navigate(new Semanas.Pag_sem6());
-        }
-
-        private void Button_sem7(object sender, RoutedEventArgs e)
-        {
-            Semanas_Frame1.NavigationService.Navigate(new Semanas.Pag_sem7());
-        }
-
-        private void Button_sem8(object sender, RoutedEventArgs e)
-        {
-            Semanas_Frame1.NavigationService.Navigate(new Semanas.Pag_sem8());
-        }
-
-        private void Button_sem9(object sender, RoutedEventArgs e)
-        {
-            Semanas_Frame1.NavigationService.Navigate(new Semanas.Pag_sem9());
-        }
-
-        private void Button_sem10(object sender, RoutedEventArgs e)
-        {
-            Semanas_Frame1.NavigationService.Navigate(new Semanas.Pag_sem10());
-        }
-
-        private void Button_sem11(object sender, RoutedEventArgs e)
-        {
-            Semanas_Frame1.NavigationService.Navigate(new Semanas.Pag_sem11());
-        }
-
-        private void Button_sem12(object sender, RoutedEventArgs e)
-        {
-            Semanas_Frame1.NavigationService.Navigate(new Semanas.Pag_sem12());
-        }
-
-        private void Button_sem13(object sender, RoutedEventArgs e)
-        {
-            Semanas_Frame1.NavigationService.Navigate(new Semanas.Pag_sem13());
-        }
-
-        private void Button_sem14(object sender, RoutedEventArgs e)
-        {
-            Semanas_Frame1.NavigationService.Navigate(new Semanas.Pag_sem14());
-        }
-
-        private void Button_sem15(object sender, RoutedEventArgs e)
-        {
-            Semanas_Frame1.NavigationService.Navigate(new Semanas.Pag_sem15());
-        }
-
-        private void Semanas1_Frame_Navigated(object sender, System.Windows.Navigation.NavigationEventArgs e)
-        {
-
+            if (cbox_semana.SelectedItem != null)
+            {
+                int semana = Convert.ToInt32(cbox_semana.SelectedItem);
+                CargarAsignacionesSemana(semana);
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -117,11 +141,6 @@ namespace AdisG3
             MainWindow mainWindow = new MainWindow();
             this.Close();
             mainWindow.Show();
-        }
-
-        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
-        {
-
         }
 
         private void Button_Nota(object sender, RoutedEventArgs e)
@@ -132,9 +151,17 @@ namespace AdisG3
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             AgregarTarea agregar_Tarea = new AgregarTarea(id_profesor, id_cursoSeleccionado);
+            agregar_Tarea.Closed += AgregarTarea_Closed; // Suscribir al evento Closed de la ventana AgregarTarea
             this.Close();
             agregar_Tarea.Show();
         }
+        private void AgregarTarea_Closed(object sender, EventArgs e)
+        {
+            // Vuelve a cargar las asignaciones de tareas después de cerrar la ventana AgregarTarea
+            CargarAsignacionesSemana(Convert.ToInt32(cbox_semana.SelectedItem));
+            lvAsignacionesSemana.Items.Refresh(); // Actualiza la vista de la lista de asignaciones de tareas
+        }
+
 
         private void BackButton_Click(object sender, RoutedEventArgs e)
         {
@@ -142,7 +169,5 @@ namespace AdisG3
             administradorInicio.Show();
             this.Close();
         }
-
-
     }
 }
