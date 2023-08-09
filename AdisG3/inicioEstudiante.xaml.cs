1d79416de4988842d02ec9a441b641e61fed0ca1
fmongec924 <114545502+fmongec924@users.noreply.github.com>
2023-08-09 16:02:51 -0600

    Interfaz general, colores de los cursos y texto de institución justificado

--- AdisG3/inicioEstudiante.xaml.cs
+++ AdisG3/inicioEstudiante.xaml.cs
@@ -100,6 +100,8 @@ namespace AdisG3
 
                     using (MySqlDataReader reader = command.ExecuteReader())
                     {
+                        Random r = new Random();
+
                         while (reader.Read())
                         {
                             idCurso = reader.GetInt32("id_curso");
@@ -144,7 +146,8 @@ namespace AdisG3
                             cursoButton.Tag = idCurso; // Almacena el ID del curso en la propiedad Tag del botón
 
                             // Establecer el fondo del botón del curso como un color sólido
-                            cursoButton.Background = new SolidColorBrush(Colors.LightBlue);
+
+                            cursoButton.Background = new SolidColorBrush(Color.FromRgb((byte)r.Next(100, 255), (byte)r.Next(100, 255), (byte)r.Next(100, 255)));
 
                             // Calcula la posición de la fila y columna en la cuadrícula
                             int fila = index / 4;
@@ -218,19 +221,13 @@ namespace AdisG3
             TextoInstitucion.Visibility = Visibility.Visible;
 
             // Establecer el texto
-            TextoInstitucion.Text = "   Para ULACIT, el aprendizaje es la capacidad de un individuo de utilizar el conocimiento en \n" +
-                                    "   situaciones novedosas (por ejemplo, solucionar problemas, \n" +
-                                    "   diseñar productos o argumentar puntos de vista),\n" +
-                                    "   de formas semejantes a las que modelan los expertos en disciplinas específicas. Los estudiantes \n" +
-                                    "   demuestran su aprendizaje cuando son capaces de ir más allá de la acumulación de información y \n" +
-                                    "   realizan proyectos que son valorados por las comunidades en que viven. Estos proyectos los llevan a \n" +
-                                    "   cabo en equipos de trabajo, utilizando las metodologías de gestión de proyectos usadas \n" +
-                                    "   en los ambientes laborales." +
-                                    "   Las lecciones se imparten en aulas invertidas: la teoría se ve en casa, \n" +
-                                    "   en las aulas virtuales; y la práctica, en el aula \n" +
-                                    "   presencial. Por tanto, el tiempo en el aula se centra en ejercicios, proyectos y discusiones, \n" +
-                                    "   no en lecciones magistrales.\n" +
-                                    "   Familiarícese con nuestro modelo educativo y metodología de enseñanza basada en proyectos.";
+            TextoInstitucion.TextAlignment = TextAlignment.Justify;
+            TextoInstitucion.Text = "   Para ULACIT, el aprendizaje es la capacidad de un individuo de utilizar el conocimiento en situaciones novedosas (por ejemplo, solucionar problemas," +
+                                    " diseñar productos o argumentar puntos de vista), de formas semejantes a las que modelan los expertos en disciplinas específicas. Los estudiantes" +
+                                    " demuestran su aprendizaje cuando son capaces de ir más allá de la acumulación de información y realizan proyectos que son valorados por las comunidades en que viven. Estos proyectos los llevan a" +
+                                    " cabo en equipos de trabajo, utilizando las metodologías de gestión de proyectos usadas en los ambientes laborales. Las lecciones se imparten en aulas invertidas: la teoría se ve en casa," +
+                                    " en las aulas virtuales; y la práctica, en el aula presencial. Por tanto, el tiempo en el aula se centra en ejercicios, proyectos y discusiones, no en lecciones magistrales" +
+                                    " Familiarícese con nuestro modelo educativo y metodología de enseñanza basada en proyectos.";
         }
 
         private void BackButton_Click(object sender, RoutedEventArgs e)
